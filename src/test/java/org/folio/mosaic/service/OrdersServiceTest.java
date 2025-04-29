899d37030a86955e578cb291fcfbaf0210352c71
Azizbek Khushvakov <azizbek_khushvakov@epam.com>
2025-04-29 15:20:08 +0500

    Fix Tests

--- src/test/java/org/folio/mosaic/service/OrdersServiceTest.java
+++ src/test/java/org/folio/mosaic/service/OrdersServiceTest.java
@@ -2,17 +2,18 @@ package org.folio.mosaic.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
 import java.util.UUID;
+
 import org.folio.mosaic.client.OrdersClient;
 import org.folio.mosaic.support.CopilotGenerated;
 import org.folio.rest.acq.model.mosaic.MosaicConfiguration;
+import org.folio.rest.acq.model.mosaic.MosaicOrder;
+import org.folio.rest.acq.model.mosaic.MosaicOrderRequest;
 import org.folio.rest.acq.model.orders.CompositePurchaseOrder;
-import org.folio.rest.acq.model.orders.OrderTemplate;
 import org.folio.rest.acq.model.orders.PoLine;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -30,74 +31,81 @@ class OrdersServiceTest {
   @Mock
   private ConfigurationService configurationService;
 
+  @Mock
+  private MosaicOrderConverter orderConverter;
+
   @InjectMocks
   private OrdersService ordersService;
 
   @Test
-  void positive_createOrder() {
-    CompositePurchaseOrder compositePurchaseOrder = new CompositePurchaseOrder();
-    compositePurchaseOrder.setPoNumber("PO12345");
-    var templateId = UUID.randomUUID();
+  void positive_createOrder_withProvidedTemplateId() {
+    String templateId = UUID.randomUUID().toString();
+    MosaicOrder mosaicOrder = new MosaicOrder().withTitle("Test Book");
+    MosaicOrderRequest request = new MosaicOrderRequest()
+      .withOrderTemplateId(templateId)
+      .withMosaicOrder(mosaicOrder);
 
+    CompositePurchaseOrder orderTemplate = new CompositePurchaseOrder();
+    CompositePurchaseOrder compositePurchaseOrder = new CompositePurchaseOrder();
     CompositePurchaseOrder createdOrder = new CompositePurchaseOrder();
     createdOrder.setPoLines(List.of(new PoLine().withPoLineNumber("POL12345")));
 
+    when(ordersClient.getOrderTemplateById(templateId)).thenReturn(orderTemplate);
+    when(orderConverter.convertToCompositePurchaseOrder(mosaicOrder, orderTemplate)).thenReturn(compositePurchaseOrder);
     when(ordersClient.createOrder(compositePurchaseOrder)).thenReturn(createdOrder);
 
-    String result = ordersService.createOrder(templateId, compositePurchaseOrder);
+    String result = ordersService.createOrder(request);
 
     assertEquals("POL12345", result);
+    verify(ordersClient).getOrderTemplateById(templateId);
+    verify(orderConverter).convertToCompositePurchaseOrder(mosaicOrder, orderTemplate);
     verify(ordersClient).createOrder(compositePurchaseOrder);
-  }
-
-  @Test
-  void positive_createOrder_useTemplateId() {
-    var orderTemplate = new OrderTemplate();
-    orderTemplate.setTemplateCode("Amazon_dot_com");
-    var templateId = UUID.randomUUID();
-
-    var createdOrder = new CompositePurchaseOrder();
-    createdOrder.setPoLines(List.of(new PoLine().withPoLineNumber("POL12345")));
-
-    when(ordersClient.getOrderTemplateById(any())).thenReturn(orderTemplate);
-    when(ordersClient.createOrder(createdOrder)).thenReturn(createdOrder);
-
-    String result = ordersService.createOrder(templateId, createdOrder);
-
-    assertEquals("POL12345", result);
-    verify(ordersClient).getOrderTemplateById(any());
   }
 
   @Test
   void positive_createOrder_useDefaultTemplate() {
-    var orderTemplate = new OrderTemplate();
-    orderTemplate.setTemplateCode("Amazon_dot_com");
+    String defaultTemplateId = UUID.randomUUID().toString();
+    MosaicOrder mosaicOrder = new MosaicOrder().withTitle("Test Book");
+    MosaicOrderRequest request = new MosaicOrderRequest()
+      .withMosaicOrder(mosaicOrder); // No template ID provided
 
-    var defaultTemplateId = UUID.randomUUID();
-
+    CompositePurchaseOrder orderTemplate = new CompositePurchaseOrder();
+    CompositePurchaseOrder compositePurchaseOrder = new CompositePurchaseOrder();
     CompositePurchaseOrder createdOrder = new CompositePurchaseOrder();
     createdOrder.setPoLines(List.of(new PoLine().withPoLineNumber("POL12345")));
 
-    when(configurationService.getConfiguration()).thenReturn(new MosaicConfiguration().withDefaultTemplateId(defaultTemplateId.toString()));
-    when(ordersClient.getOrderTemplateById(any())).thenReturn(orderTemplate);
-    when(ordersClient.createOrder(createdOrder)).thenReturn(createdOrder);
+    when(configurationService.getConfiguration()).thenReturn(new MosaicConfiguration().withDefaultTemplateId(defaultTemplateId));
+    when(ordersClient.getOrderTemplateById(defaultTemplateId)).thenReturn(orderTemplate);
+    when(orderConverter.convertToCompositePurchaseOrder(mosaicOrder, orderTemplate)).thenReturn(compositePurchaseOrder);
+    when(ordersClient.createOrder(compositePurchaseOrder)).thenReturn(createdOrder);
 
-    String result = ordersService.createOrder(null, createdOrder);
+    String result = ordersService.createOrder(request);
 
     assertEquals("POL12345", result);
-    verify(ordersClient).getOrderTemplateById(any());
+    verify(configurationService).getConfiguration();
+    verify(ordersClient).getOrderTemplateById(defaultTemplateId);
+    verify(orderConverter).convertToCompositePurchaseOrder(mosaicOrder, orderTemplate);
+    verify(ordersClient).createOrder(compositePurchaseOrder);
   }
 
   @Test
   void negative_createOrder_throwClientException() {
-    CompositePurchaseOrder compositePurchaseOrder = new CompositePurchaseOrder();
-    compositePurchaseOrder.setPoNumber("PO12345");
-    var templateId = UUID.randomUUID();
+    String templateId = UUID.randomUUID().toString();
+    MosaicOrder mosaicOrder = new MosaicOrder().withTitle("Test Book");
+    MosaicOrderRequest request = new MosaicOrderRequest()
+      .withOrderTemplateId(templateId)
+      .withMosaicOrder(mosaicOrder);
 
+    CompositePurchaseOrder orderTemplate = new CompositePurchaseOrder();
+    CompositePurchaseOrder compositePurchaseOrder = new CompositePurchaseOrder();
+
+    when(ordersClient.getOrderTemplateById(templateId)).thenReturn(orderTemplate);
+    when(orderConverter.convertToCompositePurchaseOrder(mosaicOrder, orderTemplate)).thenReturn(compositePurchaseOrder);
     when(ordersClient.createOrder(compositePurchaseOrder)).thenThrow(new RuntimeException("Order creation failed"));
 
-    assertThrows(RuntimeException.class, () -> ordersService.createOrder(templateId, compositePurchaseOrder));
+    assertThrows(RuntimeException.class, () -> ordersService.createOrder(request));
+    verify(ordersClient).getOrderTemplateById(templateId);
+    verify(orderConverter).convertToCompositePurchaseOrder(mosaicOrder, orderTemplate);
     verify(ordersClient).createOrder(compositePurchaseOrder);
   }
-
 }
