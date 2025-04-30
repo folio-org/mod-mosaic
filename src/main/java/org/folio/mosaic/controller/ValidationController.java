a9e3143a01b05bfdf9c554faf87fb84ec84d8464
BKadirkhodjaev <b.kadirkhodjaev@gmail.com>
2025-04-30 15:45:29 +0500

    [EDGORDERS-96-2]. Fix invalid schema method

--- src/main/java/org/folio/mosaic/controller/ValidationController.java
+++ src/main/java/org/folio/mosaic/controller/ValidationController.java
@@ -5,7 +5,6 @@ import lombok.extern.log4j.Log4j2;
 import org.folio.mosaic.domain.dto.MosaicValidation;
 import org.folio.mosaic.rest.resource.ValidateApi;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +18,6 @@ import static org.springframework.http.HttpStatus.OK;
 public class ValidationController implements ValidateApi {
 
   @Override
-  @GetMapping(value = "/validate")
   public ResponseEntity<MosaicValidation> getValidation() {
     var result = new MosaicValidation();
     result.setStatus(SUCCESS);
