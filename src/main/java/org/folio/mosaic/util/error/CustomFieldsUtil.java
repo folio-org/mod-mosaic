badddbc3795d9f9731185c1f2aa81d200b52c905
BKadirkhodjaev <b.kadirkhodjaev@gmail.com>
2025-06-12 14:41:59 +0500

    [EDGORDERS-101-3]. Allows passing value as an additional field of object type

--- src/main/java/org/folio/mosaic/util/error/CustomFieldsUtil.java
+++ src/main/java/org/folio/mosaic/util/error/CustomFieldsUtil.java
@@ -1,6 +1,7 @@
 package org.folio.mosaic.util.error;
 
 import lombok.experimental.UtilityClass;
+import org.apache.commons.lang3.ObjectUtils;
 import org.folio.rest.acq.model.mosaic.MosaicCustomFields;
 import org.folio.rest.acq.model.mosaic.MosaicOrder;
 import org.folio.rest.acq.model.orders.CustomFields;
@@ -8,11 +9,17 @@ import org.folio.rest.acq.model.orders.CustomFields;
 @UtilityClass
 public class CustomFieldsUtil {
 
+  private static final String VALUE = "value";
+
   public static CustomFields getCustomFieldsByEntityType(MosaicOrder mosaicOrder, MosaicCustomFields.EntityType entityType) {
     var convertedCustomFields = new CustomFields();
     mosaicOrder.getCustomFields().stream()
       .filter(field -> field.getEntityType() == entityType)
-      .forEach(customField -> convertedCustomFields.withAdditionalProperty(customField.getRefId(), customField.getValue()));
+      .filter(field -> ObjectUtils.isNotEmpty(field.getAdditionalProperties()))
+      .forEach(field -> {
+        var value = field.getAdditionalProperties().get(VALUE);
+        convertedCustomFields.withAdditionalProperty(field.getRefId(), value);
+      });
     return convertedCustomFields;
   }
 }
