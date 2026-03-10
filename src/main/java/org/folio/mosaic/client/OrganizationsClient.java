ab6ede98e25771631a049e3c7625d544bfa11b90
Saba-Zedginidze-EPAM <148070844+Saba-Zedginidze-EPAM@users.noreply.github.com>
2026-03-10 17:40:46 +0400

    [MODMO-22]: Fix organization creation call (#63)
    * [MODMO-22]: Fix organization creation call
    
    * [MODMO-22]: Fix order template call

--- src/main/java/org/folio/mosaic/client/OrganizationsClient.java
+++ src/main/java/org/folio/mosaic/client/OrganizationsClient.java
@@ -2,6 +2,7 @@ package org.folio.mosaic.client;
 
 import org.folio.rest.acq.model.orgs.Organization;
 import org.folio.rest.acq.model.orgs.OrganizationCollection;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
@@ -14,5 +15,5 @@ public interface OrganizationsClient {
   OrganizationCollection getOrganizations(@RequestParam("query") String query);
 
   @PostExchange(value = "/organizations")
-  void createOrganization(Organization organization);
+  void createOrganization(@RequestBody Organization organization);
 }
